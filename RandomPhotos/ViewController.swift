4b025c116b3137b07b19953ce169c62446927959
Ho Hien <hien.ho@c2c-techhub.io>
2025-10-18 22:51:42 +0700

    Initial Commit

---
+++ RandomPhotos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomPhotos
+//
+//  Created by Ho Hien on 18/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
